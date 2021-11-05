8bf23740b363a4a43d48da7d634a54ba914a2c4b
Jason Hargrove <rijaha@gmail.com>
2021-11-05 08:25:42 -0500

    Logs and section recap.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-// Logs and Section Recap
-console.log('Hello');
+// console.log everyting.
+const log = console.log;
+
+// ===== Logs and Section Recap =====
+// ↓↓↓↓↓ Bad ↓↓↓↓↓
+// O(n ** 2)
+// O(nlog n)
+// O(n)
+// O(log n)
+// O(1)
+// ↑↑↑↑↑ Good ↑↑↑↑↑
+
+// log(2 ** 2);
+// log(Math.pow(2, 2));
+
+// ===== Analyzing Preformance of Array and Objects =====
