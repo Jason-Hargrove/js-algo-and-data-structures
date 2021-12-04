ceb055e35457cb9a7550f44743506b6fd965ad73
Jason Hargrove <rijaha@gmail.com>
2021-12-04 11:28:04 -0600

    Refactored function replacing for loop with for of, shortened if else statement, replaced regex with mathematical comparison using character code.

--- index.js
+++ index.js
@@ -173,23 +173,100 @@ function charCount(str) {
 // Solve The Problem - if you can't Solve a Simpler Problem!
 // ===============================================================
 
+// function charCount(str) {
+//     // make object to return at end.
+//     let result = {};
+//     // loop over string, for each character...
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i].toLowerCase();
+//         // if the char is a number/letter AND is a key in object, add one to count.
+//         if (result[char] > 0) {
+//             result[char]++;
+//         // if the char is a number/letter AND not in object, add it to the object and set value to 1.
+//         } else {
+//             result[char] = 1;
+//         };
+//     }
+//     // if character is something else (space, period, ect.) don't do anything.
+//     // return object at end.
+//     return result;
+// }
+// log(charCount('Hi There!'))
+
+// ===============================================================
+// * Look Back & Refactor
+// Try to improve your code.
+// ===============================================================
+
+// Can you check the result?
+// Can you derive the result differently?
+// Can you understand it at a glance?
+// Can you use the result or method fo some other problem?
+// Can you improve the performance of you solution?
+// Can you think of other ways to refactor?
+// How have other people solved this problem?
+
+// function charCount(str) {
+//     // make object to return at end.
+//     let obj = {};
+//     // loop over string, for each character...
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i].toLowerCase();
+//         // if the char is a number/letter AND is a key in object, add one to count.
+//         if (/[a-z0-9]/.test(char)) {
+//             if (obj[char] > 0) {
+//                 obj[char]++;
+//                 // if the char is a number/letter AND not in object, add it to the object and set value to 1.
+//             } else {
+//                 obj[char] = 1;
+//             };
+//         }
+//     }
+//     // if character is something else (space, period, ect.) don't do anything.
+//     // return object at end.
+//     return obj;
+// }
+
+// ===== Can Refactor =====
+// function charCount(str) {
+//     let obj = {};
+//     // Refactor with a For Of loop.
+//     for (let char of str) {
+//         char = char.toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//         // Refactor if else with...
+//             obj[char] = ++obj[char] || 1;
+//         }
+//     }
+//     return obj;
+// }
+
+// ===== Are Regular Expressions Efficient? =====
+// Other ways to handle the situation?
+log("hi".charCodeAt(0));
+log("hi".charCodeAt(1));
+log("j".charCodeAt(0));
+
 function charCount(str) {
-    // make object to return at end.
-    let result = {};
-    // loop over string, for each character...
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        // if the char is a number/letter AND is a key in object, add one to count.
-        if (result[char] > 0) {
-            result[char]++;
-        // if the char is a number/letter AND not in object, add it to the object and set value to 1.
-        } else {
-            result[char] = 1;
+    let obj = {};
+    for (let char of str) {
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase();
+            obj[char] = ++obj[char] || 1;
         }
     }
-    // if character is something else (space, period, ect.) don't do anything.
-    // return object at end.
-    return result;
+    return obj;
 }
-log(charCount('hello'))
 
+// Replace Regular Expression with mathematical comparison with character code.
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+       return false;
+    }
+    return true;
+}
+
+log(charCount('Hello hi!'));
